af901c5b22ecb328b920533a96bf4de5c7c0153d
madhur53 <madhurnchaudhari@gmail.com>
2025-12-29 17:27:18 +0530

    Update

---
+++ SPRINGBOOTMVC-STUDENT-JPA/src/main/java/com/example/demo/SpringbootmvcStudentJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcStudentJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcStudentJpaApplication.class, args);
+	}
+
+}
---
+++ SPRINGBOOTMVC-STUDENT-JPA/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+	private final StudentRepository studentRepository;
+	
+	public StudentController(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+	
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		model.addAttribute("student", new Student());
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String registerStudent(@ModelAttribute Student student, Model model) {
+		studentRepository.save(student);
+		model.addAttribute("students", studentRepository.findAll());
+		return "students";
+	}
+}
